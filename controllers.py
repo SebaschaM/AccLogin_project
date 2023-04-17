bb7b6e8e97065fb06220b06b89fa409f8179e609
KennethHernan <kdelacruz18@autonoma.edu.pe>
2023-04-16 21:18:32 -0500

    [Controller]

--- controllers.py
+++ controllers.py
@@ -1,1 +1,37 @@
-# Aqui van los controladores
+from flask import session, redirect, url_for
+from models import User
+
+def register_user(name, email, password):
+    # Validar que el correo electrónico no esté registrado previamente
+    if User.query.filter_by(email=email).first():
+        return False, 'El correo electrónico ya está registrado'
+
+    # Validar que la contraseña tenga al menos 8 caracteres
+    if len(password) < 8:
+        return False, 'La contraseña debe tener al menos 8 caracteres'
+
+    # Crear un nuevo usuario y guardarlo en la base de datos
+    user = User(name=name, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return True, 'Usuario registrado correctamente'
+
+
+def login_user(email, password):
+    # Buscar al usuario por correo electrónico
+    user = User.query.filter_by(email=email).first()
+
+    # Verificar que el usuario existe y que la contraseña es correcta
+    if user and user.check_password(password):
+        # Crear una sesión de usuario
+        session['user_id'] = user.id
+        return True, 'Inicio de sesión exitoso'
+
+    return False, 'Correo electrónico o contraseña incorrectos'
+
+
+def logout_user():
+    # Eliminar la sesión de usuario
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
